b67b684b8d6b9cbf3ead01889b0dcd00f0115f39
Freesu <freeu645@gmail.com>
2024-03-18 09:47:54 +0800

    3rd commit

---
+++ 2nd term/Finals/Module7 File Manipulation/Alba_Filemanip.cpp
@@ -0,0 +1,220 @@
+#include <iostream> 
+#include <fstream> 
+#include <string>
+
+using namespace std;
+
+struct Player {
+    string nickname;
+    int age;
+    double scores[5];
+};
+
+struct Node {
+    Player player;
+    Node * next;
+};
+ 
+Node * head = nullptr;
+
+void addRecord(const Player& player);
+void viewRecords();
+void computeAverage();
+void showMaxAverage();
+void showMinAverage();
+void openFile();
+void saveRecord();
+void closeFile();
+
+int main (){
+    int choice;
+    Player P1;
+    bool start = true;
+    while (start){
+        cout << "==============================================" << endl;
+        cout << "                    MENU" << endl;
+        cout << "==============================================" << endl;
+        cout << "1. Add record" << endl;
+        cout << "2. View players records" << endl;
+        cout << "3. Compute for the average" << endl;
+        cout << "4. Show the player(s) who gets the max average." << endl;
+        cout << "5. Show the player(s) who gets the min average." << endl;
+        cout << "6. Save Record to file" << endl;
+        cout << "7. Open the file." << endl;
+        cout << "8. Close the File" << endl;
+        cout << "9. Exit" << endl;
+        cin >> choice;
+        switch (choice){
+            case 1:
+                cout << "Enter nickname: ";
+                cin >> P1.nickname;
+                cout << endl << "Enter age";
+                cin >> P1.age;
+                for (int n = 0; n < 5; n++){
+                    cout <<endl<< "Enter score " << n;
+                    cin >> P1.scores[n];
+                }
+                addRecord(P1);
+                break;
+            case 2:
+                viewRecords();
+                break;
+            case 3:
+                computeAverage();
+                break;
+            case 4:
+                showMaxAverage();
+                break;
+            case 5:
+                showMinAverage();
+                break;
+            case 6:
+                saveRecord();
+                break;
+            case 7:
+                openFile();
+                break;
+            case 8:
+                closeFile();
+                break;
+            case 9:
+                start = false;
+                break;
+            default:
+                cout << "Wrong input try again!";
+                break;
+        }        
+        
+    }
+}
+
+void addRecord(const Player& player) { 
+    Node* newNode = new Node; 
+    newNode->player = player;
+    newNode->next = head; 
+    head = newNode;
+}
+
+void viewRecords() { 
+    Node* current = head;
+    while (current != nullptr) {
+        cout << "Nickname: " << current->player.nickname << endl; 
+        cout << "Age: " << current->player.age << endl;
+        cout << "Scores: ";
+        for (int i = 0; i < 5; ++i) {
+            cout << current->player.scores[i] << " ";
+        }
+        cout << endl;
+        cout << "	" << endl;
+        current = current->next;
+    }   
+}
+
+void computeAverage() { 
+    Node* current = head; 
+    while (current != nullptr) {
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += current->player.scores[i];
+        }
+        double average = sum / 5.0;
+        cout << "Nickname: " << current->player.nickname << endl; 
+        cout << "Average Score: " << average << endl;
+        cout << "	" << endl;
+        current = current->next;
+    }
+}
+
+void showMaxAverage() { 
+    Node* current = head;
+    double maxAverage = -1; 
+    while (current != nullptr) {
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += current->player.scores[i];
+        }
+        double average = sum / 5.0; 
+        if (average > maxAverage) { 
+                maxAverage = average;
+        }
+        current = current->next;
+    }
+    current = head;
+    while (current != nullptr) { 
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += current->player.scores[i];
+        }
+        double average = sum / 5.0; 
+        if (average == maxAverage) {
+            cout << "Nickname: " << current->player.nickname << endl; 
+            cout << "Average Score: " << average << endl;
+            cout << "	" << endl;
+        }
+        current = current->next;
+    }
+}
+
+void showMinAverage() { 
+    Node* current = head;
+    double minAverage = 100;
+    while (current != nullptr) {
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += current->player.scores[i];
+        }
+        double average = sum / 5.0; 
+        if (average < minAverage) { 
+            minAverage = average;
+        }
+        current = current->next;
+    }
+    current = head;
+    while (current != nullptr) { 
+        double sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += current->player.scores[i];
+        }
+        double average = sum / 5.0; 
+        if (average == minAverage) {
+            cout << "Nickname: " << current->player.nickname << endl; 
+            cout << "Average Score: " << average << endl;
+            cout << "	" << endl;
+        }
+    current = current->next;
+    }
+}
+
+void saveRecord() {
+    ofstream outFile("player_records.txt", fstream::app);
+    Node* current = head;
+    while (current != NULL) {
+        outFile << current->player.nickname << endl;
+        outFile << current->player.age << endl;
+        for (int i = 0; i < 5; ++i) {
+            outFile << current->player.scores[i] << " ";
+        }
+        outFile << endl;
+        current = current->next;
+        cout << "------------------------------- \n\n";
+    }
+    outFile.close();
+    cout << "Records saved to 'player_records.txt'." << endl;
+}
+
+void closeFile() {
+ cout << "File closed." << endl;
+}
+
+void openFile() {
+	string line;
+	ifstream myfile ("player_records.txt");
+	if (myfile.is_open()) {
+		while (getline (myfile, line)) {
+			cout << line << '\n';	
+		}
+		myfile.close();
+	}
+	
+	else cout << "Unable to open file";
+}
